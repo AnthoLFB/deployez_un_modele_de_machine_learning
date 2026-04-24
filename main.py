39e0ba23e6af2193e54256cb026ed060aa4ae109
AnthoLFB <83961647+AnthoLFB@users.noreply.github.com>
2026-04-24 13:57:37 +0200

    Ajout de la configuration de base et de la CI/CD. (#3)
    * Configuration de base et Ajout de la partie CI (intégration continue) (#1)
    
    * Ajout des fichiers de base.
    
    * Mise en place du fichier gitignore.
    
    * Mise en place d'UV pour la gestion des dépendances et ajout de pytest.
    
    * Ajout d'un test basique pour vérifier le fonctionnement de pytest.
    
    * Configuration de la CI sur les branche main et develop.
    
    * Correction de l'indentation et des caractères spéciaux
    
    * Ajout de dépendances pour la sécurité, le formatage et la vérification de l'intégrité du code.
    
    * Configuration des dossiers à exclure lors de la vérification.
    
    * Configuration des variables d'environnement.
    
    * Formatage du code via Black.
    
    * Ajout de dépendances pour la sécurité, le formatage et la vérification de l'intégrité du code.
    
    * Ajout de la vérification de code dans la CI via Black et Flake8.
    
    * Merge pull request #2 from AnthoLFB/add_cd
    
    * Ajout de la partie CD (Déploiement continue) vers Hugging Face

---
+++ main.py
@@ -0,0 +1,15 @@
+# Imports
+import os
+from dotenv import load_dotenv
+
+# Récupération des variables d'environnement
+load_dotenv("configuration/.env")
+
+
+# Initialisation du programme
+def main():
+    print(f"Bonjour ! Bienvenue sur le projet : {os.getenv("APP_NAME")}")
+
+
+if __name__ == "__main__":
+    main()
