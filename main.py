3933b9f803df021ed95103c520a228603714ae09
= <=>
2026-04-24 14:49:17 +0200

    Correction guillemets suite à une erreur de retournée par la CI.

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ load_dotenv("configuration/.env")
 
 # Initialisation du programme
 def main():
-    print(f"Bonjour ! Bienvenue sur le projet : {os.getenv("APP_NAME")}")
-
+    print(f"Bonjour ! Bienvenue sur le projet : {os.getenv('APP_NAME')}")
 
 if __name__ == "__main__":
     main()
