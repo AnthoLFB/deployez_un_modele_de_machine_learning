79c246feee1e310abbd1085221d7438e9e68c59e
Antho <anthony.lefebvre@culture-formation.fr>
2026-06-09 11:21:47 +0200

    Amélioration et correction de la CD.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 # Pour lancer l'application  : uvicorn app.main:app
+from app.main import app
 
 if __name__ == "__main__":
     import uvicorn
-    from app.main import app
-
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=7860)
